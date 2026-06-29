560c97a856b717347a4923ba9dba84303d9ccaf6
Rutuja Pachorkar <pachorkarrutuja91@gmail.com>
2026-06-29 12:31:50 +0530

    ip assigned

--- server/routes/labPresenceRoutes.js
+++ server/routes/labPresenceRoutes.js
@@ -6,12 +6,13 @@ const {
 } = require('../controllers/labPresenceController');
 const { requireAuth } = require('../middleware/auth');
 const { requireRole } = require('../middleware/role');
+const { requireCollegeNetwork } = require('../middleware/network');
 
 const router = express.Router();
 router.use(requireAuth);
 
 router.get('/me/current',  requireRole('student'), myCurrent);
 router.get('/me/history',  requireRole('student'), myHistory);
-router.post('/:id/check-out', requireRole('student'), checkOut);
+router.post('/:id/check-out', requireRole('student'), requireCollegeNetwork, checkOut);
 
 module.exports = router;
